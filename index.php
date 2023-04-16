50be5b098cb379f3265b978813d9099ffbd17b30
Murilo Sopi <dev.murilosopi@gmail.com>
2023-04-16 12:59:38 -0300

    database com illuminate

--- index.php
+++ index.php
@@ -2,6 +2,7 @@
 
 use \Psr\Http\Message\ServerRequestInterface as Request;
 use \Psr\Http\Message\ResponseInterface as Response;
+use Illuminate\Database\Capsule\Manager as Capsule;
 
 require 'vendor/autoload.php';
 
@@ -11,30 +12,66 @@ $app = new \Slim\App([
     ]
 ]);
 
-// Middleware: camada de execução antes e depois do core da aplicação (neste caso, as rotas)
+// Database
+$container = $app->getContainer();
 
-$app->add(function($request, $response, $next) {
-    $response->write('Middleware 1 + ');
-    // return $next($request, $response); # Apenas uma validação, seguida pela execução da rota
+// Illuminate -> query builder
+$container['db'] = function() {
+    $capsule = new Capsule;
 
-    $response = $next($request, $response); # Executa a rota
+    $capsule->addConnection([
+        'driver' => 'mysql',
+        'host' => 'localhost',
+        'database' => 'slim',
+        'username' => 'root',
+        'password' => '',
+        'charset' => 'utf8',
+        'collation' => 'utf8_unicode_ci',
+        'prefix' => '',
+    ]);
 
-    return $response->write('+ Fim middleware 1');
-});
+    $capsule->setAsGlobal();
+    $capsule->bootEloquent();
 
-/* O primeiro e último middleware a ser executado
-$app->add(function($request, $response, $next) {
-    $response->write('Middleware 2 + ');
-    return $next($request, $response);
-});
-*/
+    return $capsule;
+};
 
-$app->get('/usuarios', function($request, $response) {
-    $response->write('Ação principal - <strong>Usuários</strong>: ');
-});
+$app->get('/usuarios', function(Request $request, Response $response) {
+    # CREATE - Criação da tabela 'usuarios':
+    $db = $this->db->schema();
+    $db->dropIfExists('usuarios');
+    $db->create('usuarios', function($table) {
+        $table->increments('id');
+        $table->string('nome');
+        $table->string('email');
+        $table->timestamps();
+    });
 
-$app->get('/postagens', function($request, $response) {
-    $response->write('Ação principal - <strong>Postagens</strong>: ');
+    # INSERT INTO - Inserção de usuários
+    $db = $this->db;
+    $db->table('usuarios')->insert([
+        'nome' => 'Murilo Sousa Pinheiro',
+        'email' => 'murilo@gmail.com',
+    ]);
+
+    # UPDATE - Atualização de registros da tabela 'usuarios'    
+    $db = $this->db;
+    $db->table('usuarios')->where('id', 1)
+                          ->update(['nome' => 'Murilo Sopi']);
+
+    # DELETE FROM - Deletar usuarios
+    $db = $this->db;
+    $db->table('usuarios')
+            ->where('id', 1)
+            ->delete();
+    
+    # SELECT * FROM - Listar usuarios
+    $tUsuarios = $this->db->table('usuarios');
+    foreach($tUsuarios->get() as $u) {
+        echo "$u->id - $u->nome - $u->email <br>";
+    };
+
+
 });
 
 $app->run();
